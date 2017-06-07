594da09accf7869318a53c701748574ee430056e
YanRamos <yanramos78@gmail.com>
2017-06-07 20:28:38 -0300

    Enviando para o github de novo

---
+++ src/main/java/yan/scm/App.java
@@ -0,0 +1,13 @@
+package yan.scm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Ola mundo!" );
+    }
+}  
